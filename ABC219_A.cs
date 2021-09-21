761541562805c883296c95b53216c9c68ac45b37
mkawauchi <90766715+mkawuchi@users.noreply.github.com>
2021-09-21 23:56:40 +0900

    first submission

--- ABC219_A.cs
+++ ABC219_A.cs
@@ -1,28 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 class Program
 {
     static void Main()
     {
-        var Score = int.Parse(Console.ReadLine());
-        var Ans = string.Empty;
+        string input = Console.ReadLine();
 
-        if (0 <= Score && Score < 40)
+        if (string.IsNullOrEmpty(input))
         {
-            Ans = (40-Score).ToString();
+            Console.WriteLine("値が入力されていません");
+            return;
         }
-        else if (Score < 70)
-        {
-            Ans = (70-Score).ToString();
-        }
-        else if (Score < 90)
-        {
-            Ans = (90-Score).ToString();
-        }
+        
         else
         {
-            Ans = "expert";
+            int Score;
+            if (!int.TryParse(input, out Score))
+            {
+                Console.WriteLine("整数が入力されていません");
+            }
+
+            else
+            {
+                if (Score < 0 || Score > 100)
+                {
+                    Console.WriteLine("範囲外の値が入力されています");
+                }
+
+                else if (0 <= Score && Score < 40)
+                {
+                    Console.WriteLine((40-Score).ToString());
+                }
+
+                else if (Score < 70)
+                {
+                    Console.WriteLine((70-Score).ToString());
+                }
+                else if (Score < 90)
+                {
+                    Console.WriteLine((90-Score).ToString());
+                }
+                else
+                {
+                    Console.WriteLine("expert");
+                }
+
+            }
         }
-        Console.WriteLine(Ans);
+        
     }
 }
